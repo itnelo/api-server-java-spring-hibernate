4cc452a718f5172d0eecc5f2b5fe0d734eb325da
Pavel Petrov <nelo@guildmaker.ru>
2015-10-06 01:24:43 +0400

    added Debug Aspect advice

--- src/main/java/api/Application.java
+++ src/main/java/api/Application.java
@@ -1,16 +1,15 @@
-
 package api;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@ComponentScan({"api"})
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
